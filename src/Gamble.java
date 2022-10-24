4a5f8dc9a4706c4f6cc58263dfa82d0f6c8c0579
ChasmY <claudiusm124@gmail.com>
2022-10-24 20:09:52 +0300

    Second

--- src/Gamble.java
+++ src/Gamble.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Gamble {
     public AvailableGames name;
-    public int table = 0, playersAllowed = 0, playersEnrolled = 0;
-
+    public int table = 0, playersAllowed = 0;
+    public List<Client> clientsList = new ArrayList<Client>();
+    public Dealer dealer;
 }
